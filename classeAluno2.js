9cc7bbd4f7f13434aefa3b7541f51c394bbc122a
Fernando Felipe <fernando.silva.ribeiro@escola.pr.gov.br>
2026-04-15 12:45:38 +0000

    ok

--- classeAluno2.js
+++ classeAluno2.js
@@ -25,12 +25,28 @@ class Aluno{
         } else{
             return "reprovado"
         }
-exibirRelatorio(){
-    console.log("")
-}
+
 
     }
 }
-let aluno1 = new Aluno("Luiz", 850850, [7.2, 6.5, 3.3])
-console.log(aluno1.calcularMedia());
-console.log(aluno1.obterStatus());
+
+let turma = [
+    new Aluno(`Luiz`, `CGM5050`, [7.2, 2.5, 1.1]),
+    new Aluno(`Ana`,  `CGM5051`, [7.3, 2.5, 6.1]),
+    new Aluno(`Renan`, `CGM5052`, [4.4, 3.5, 1.9]),
+    new Aluno(`Fernando`, `CGM5053`, [9.6, 3.5, 7.5]),
+    new Aluno(`Mathheus`, `CGM5054`, [7.0, 5.5, 3.8]),
+    new Aluno(`Kauan`, `CGM5055`, [7.6, 8.5, 5.5]),
+]
+
+console.log(turma[1]);
+
+
+
+
+
+
+//let aluno1 = new Aluno("Luiz", 850850, [7.2, 6.5, 3.3])
+//console.log(aluno1.calcularMedia());
+//console.log(aluno1.obterStatus());
+
